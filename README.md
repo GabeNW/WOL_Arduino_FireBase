56e608069c8dd8a1e3abad89ee013de0b74357cb
Gabriel Leme <gabriellemenw@gmail.com>
2024-08-08 23:03:59 -0300

    Code Rebuild
    The libraries were updated, which resulted in the code being redone, using new methods and removing unnecessary things.

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
 # WOL_Arduino_Firebase
-Wake On Lan with Firebase and ESP32
+This code is made to turn your pc on using your arduino (I used an ESP32) with the Wake on lan function and the Firebase database.
 
 Explanation:
-This code is to turn your pc on using an arduino and the Firebase database.
-To use it just modify the script with the following information and you can already upload it to your ESP32 (I didn't have an ESP8266, so I didn't test it, but it should work).
-The info you'll need is:
+To use it just modify the script with your information and you can already upload it to your arduino (If you have some trouble, look into the "FirebaseClient" library documentation).
+The info you need to change is:
 
 "ssid": Your WiFi name.
 "password": Self-explanatory.
-"firebHost": Your firebase database link (I'll not explain here how to create a firebase project).
+"firebHost": Your firebase database link (I'll not explain here how to create a firebase project, you can find how searching on youtube or in the "FirebaseClient" library documentation).
 "API_KEY": Your firebase API key project.
-"MACAddress": Self-explanatory.
+"USER_EMAIL": Your email attached to your firebase client (I'll also not explain here how to create an email to authenticate to your firebase project, this is another thing you can search on youtube or the oficial documentation).
+"USER_PASSWORD": Self-explanatory.
+"MACAddress": The Mac Address of your device.
 
 Just open the .ino file, find the variables and change it.
-Once uploaded to your ESP32, just access your database and change the "activate" (or whatever name you use it) variable to "1", and the magic package is sent.
+Once uploaded to your arduino, just access your database and change your variable to "1", and the magic package is sent.
 
 Notes:
-Keep in mind you have to keep your PC connected to a power source and have it turned on at least once, otherwise the magic packet won't work.
+Keep in mind you have to keep your PC connected to a power source and have it turned on at least once, otherwise the packet won't work.
+If the package is not sent, try changing the WOL port number (7 or 9 should work).
+Have fun.
--- Wake_On_Lan_FireBase.ino
+++ Wake_On_Lan_FireBase.ino
@@ -1,119 +1,191 @@
-#include <WiFi.h>
-#include<Firebase_ESP_Client.h>
-#include <WiFiUDP.h>
 #include <WakeOnLan.h>
-#include "addons/TokenHelper.h"
-#include "addons/RTDBHelper.h"
+#include <FirebaseClient.h>
 
+//Defining the correct libraries
+#if defined(ESP32) || defined(ARDUINO_RASPBERRY_PI_PICO_W) || defined(ARDUINO_GIGA)
+#include <WiFi.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#elif __has_include(<WiFiNINA.h>) || defined(ARDUINO_NANO_RP2040_CONNECT)
+#include <WiFiNINA.h>
+#elif __has_include(<WiFi101.h>)
+#include <WiFi101.h>
+#elif __has_include(<WiFiS3.h>) || defined(ARDUINO_UNOWIFIR4)
+#include <WiFiS3.h>
+#elif __has_include(<WiFiC3.h>) || defined(ARDUINO_PORTENTA_C33)
+#include <WiFiC3.h>
+#elif __has_include(<WiFi.h>)
+#include <WiFi.h>
+#endif
+
+//Defining the correct WiFi client
+#if defined(ESP32) || defined(ESP8266) || defined(ARDUINO_RASPBERRY_PI_PICO_W)
+#include <WiFiClientSecure.h>
+WiFiClientSecure ssl_client;
+#elif defined(ARDUINO_ARCH_SAMD) || defined(ARDUINO_UNOWIFIR4) || defined(ARDUINO_GIGA) || defined(ARDUINO_PORTENTA_C33) || defined(ARDUINO_NANO_RP2040_CONNECT)
+#include <WiFiSSLClient.h>
+WiFiSSLClient ssl_client;
+#endif
+
+//Your Changes Here
 #define ssid "YOUR NETWORK NAME"
 #define password "YOUR PASSWORD"
 #define firebHost "https://exemple.firebaseio.com/"
 #define API_KEY "PDjeifPEKFOdt_PDjeifPEKFOdt-uYTRaxy_YOP"
+#define USER_EMAIL "youremail@gmail.com"
+#define USER_PASSWORD "yourpassword"
 
-activate = 0;
-
-FirebaseData fbdo;
-FirebaseAuth auth;
-FirebaseConfig config;
-
-unsigned long sendDataPrevMillis = 0;
-bool signupOk = false;
+//Firebase stuff
+using AsyncClient = AsyncClientClass;                       //Define the AsyncClient class as the one to be used
+//Firebase Objects
+FirebaseApp app;
+AsyncResult aResult;
+DefaultNetwork network;
+RealtimeDatabase Database;
+AsyncClient aClient(ssl_client, getNetwork(network));       //Create a client using the WiFiClientSecure object and the network object
+UserAuth user_auth(API_KEY, USER_EMAIL, USER_PASSWORD);     //Create a user authentication object
 
 //WOL
 WiFiUDP UDP;
-const char *MACAddress = "01:a2:34:5b:6c:7d";
-WakeOnLan WOL(UDP);
-void sendWOL();
+const char *MACAddress = "01:a2:34:5b:6c:7d";               //MAC Address of the device to be turned on
+WakeOnLan WOL(UDP);                                         //Create a WakeOnLan object
 
-void setup() {
-  //Serial
+//Usefull variables
+unsigned long lastMillis = 0;                               //Variable to store the last time the task was executed
+const int maxReconnectAttempts = 6;                         //Variable to store the maximum number of reconnection attempts
+
+void setup() 
+{
+  //Serial Initialization
   Serial.begin(115200);
-  Serial.print("ESP32 Initialized");
+  Serial.print("Arduino Initialized");
   
-  //WiFi
+  //WiFi Manager
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi");
+  
+  //Resset WiFi
+  WifiReset();
 
-  //Resset
-  int attempts = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    trespontos();
-    if(attempts > 5) {
-      ESP.restart();
-    }
-    attempts++;
-  }
-
-  //+WiFi
+  //WiFi Feedback
   Serial.println("\nConnected to the WiFi network");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
-  //Firebase ESP Client
-  config.api_key = API_KEY;
-  config.database_url = firebHost;
-  if(Firebase.signUp(&config, &auth, "", "")) {
-    Serial.println("signUp Ok");
-    signupOk = true;
-  }
-  else {
-    Serial.printf("%s\n", config.signer.signupError.message.c_str());
-  }
-  config.token_status_callback = tokenStatusCallback;
-  Firebase.begin(&config, &auth);
-  Firebase.reconnectWiFi(true);
+  //Firebase Setup
+  Firebase.printf("Firebase Client v%s\n", FIREBASE_CLIENT_VERSION);  //Print the version of the Firebase library
+  Serial.println("Initializing app...");
+
+
+#if defined(ESP32) || defined(ESP8266) || defined(PICO_RP2040)        //Set the client to accept any SSL certificate (for test purposes only)
+    ssl_client.setInsecure();
+#if defined(ESP8266)
+    ssl_client.setBufferSizes(4096, 1024);
+#endif
+#endif
+
+  initializeApp(aClient, app, getAuth(user_auth), aResult);           //Initialize the Firebase app with the created user_auth object
+  app.getApp<RealtimeDatabase>(Database);                             //Get the Realtime Database
+  Database.url(firebHost);                                            //Set the database URL
 }
 
-void loop() {
-  if (WiFi.status() != WL_CONNECTED) {
-    ESP.restart();
-  }
-  else{
-    //Saving sensor data from ESP32 to Firebase RTDB
-    if (Firebase.ready() && signupOk && (millis() - sendDataPrevMillis > 2000 || sendDataPrevMillis == 0)) 
+void loop() 
+{
+  //Firebase process
+  app.loop();                                                        //Process the incoming data and events and make the connection alive
+  Database.loop();                                                   //Process writing and reading data from the database
+  
+  //Check each second
+  if (millis() - lastMillis >= 1000)
+  {
+    lastMillis = millis();
+    //Verify if the connection is alive
+    if (WiFi.status() == WL_CONNECTED)
     {
-      sendDataPrevMillis = millis();
-      //Read and send
-      SendData();
+      //User FeedBack
+      Serial.print("Ready - ");
+
+      //Get the value from the database and store it in aResult
+      Database.get(aClient, "/YourLocalWOL/YourVar", aResult);
+    }
+    else 
+    {
+      //Reset WiFi
+      WifiReset();
     }
   }
-}
 
-void SendData() {
-  if(Firebase.RTDB.getInt(&fbdo, "/WOL")) {
-    //Serial.println("Successful Read from " + fbdo.dataPath() + ": " + activate + " (" + fbdo.dataType() + ")");    
-    if(fbdo.dataType() == "int") {
-      activate = fbdo.intData();
-      //Serial.println("Successful Read from " + fbdo.dataPath() + ": " + activate + " (" + fbdo.dataType() + ")");
-      if (activate == 1){        
-        //Send WOL
-        WOL.sendMagicPacket(MACAddress, 7);
+  if (aResult.available()) 
+  {
+    //aResult is the AsyncResult object. It is used to store the result of the get() function
+    RealtimeDatabaseResult &databaseResult = aResult.to<RealtimeDatabaseResult>();
 
-        //Send User FeedBack
-        Serial.println("Sent");
+    //Store the value in aResult in a int variable
+    int value = databaseResult.to<int>();
 
-        //Reset value
-        activate = 0;
+    //Send user FeedBack
+    Serial.print("Value: ");
+    Serial.println(value);
 
-        //Set new Data (Send new Values)
-        if(Firebase.RTDB.setInt(&fbdo, "/WOL", activate)) {
-          //Serial.println(); Serial.print(activate);
-          //Serial.println(" - successfully saved to: " + fbdo.dataPath());
-        }
-        else {Serial.println("Failed 1: " + fbdo.errorReason());}
+    if (value == 1) 
+    {
+      //Send WOL
+      bool wake = WOL.sendMagicPacket(MACAddress, 7);
+      if (wake)
+      {
+        //Set the database value to 0 and store it in aResult
+        Database.set<int>(aClient, "/YourLocalWOL/YourVar", 0, aResult);
+
+        //Send user FeedBack
+        Serial.println("WOL Sent");
       }
       else 
-        Serial.println("Reading completed, no changes found");
+      {
+        //Set the database value to 0 and store it in aResult
+        Database.set<int>(aClient, "/YourLocalWOL/YourVar", 0, aResult);
+
+        //Send user FeedBack
+        Serial.println("WOL Failed");
+      }
+      
     }
   }
-  else {Serial.println("Failed 2: " + fbdo.errorReason());}
+  
 }
 
-void trespontos() {
-  delay(500);
+//Unnecessary functions
+void WifiReset()
+{
+  int attempts = 0;
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    WiFi.disconnect();
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting");
+    Dots();
+    if(attempts > maxReconnectAttempts) 
+    {
+      Restart();
+    }
+    attempts++;
+  }
+}
+
+void Dots() 
+{
+  //Function to print dots while waiting
+  delay(300);
   Serial.print(".");
-  delay(500);
+  delay(300);
   Serial.print(".");
-  delay(500);
+  delay(300);
   Serial.println(".");
+}
+
+void Restart() 
+{
+  //Function to restart the Arduino
+  Serial.println("Restarting");
+  delay(2000);        //Optional delay before restart
+  ESP.restart();      //Restart the Arduino
 }
